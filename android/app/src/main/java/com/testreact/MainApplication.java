d3b420b885d09165c4f65d2480450bdf798d43e5
DESKTOP-M6UG5B0\oneza <karit.sittisak@gmail.com>
2023-04-09 19:12:13 +0700

    +แก้ TEXT ขยายเองจากภายนอก +responsive จอหลายขนาด

--- android/app/src/main/java/com/testreact/MainApplication.java
+++ android/app/src/main/java/com/testreact/MainApplication.java
@@ -9,6 +9,10 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import android.view.WindowManager;
+import android.content.res.Configuration;
+import android.content.Context;
+import android.util.DisplayMetrics;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -52,6 +56,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    adjustFontScale(getApplicationContext(),getResources().getConfiguration());
     SoLoader.init(this, /* native exopackage */ false);
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
@@ -59,4 +64,14 @@ public class MainApplication extends Application implements ReactApplication {
     }
     ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
+  public void adjustFontScale(Context context, Configuration configuration) {
+    if (configuration.fontScale != 1) {
+        configuration.fontScale = 1;
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(metrics);
+        metrics.scaledDensity = configuration.fontScale * metrics.density;
+        context.getResources().updateConfiguration(configuration, metrics);
+    }
+}
 }
